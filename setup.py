3eac153448c03e447c7043e2c4ccd5cee10005c2
DrDarkShadow <gummyreaker@gmail.com>
2025-01-11 11:40:39 +0530

    Folder Structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Prateek Gaur',
+    author_email= 'gummyreaker@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
